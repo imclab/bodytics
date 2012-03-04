70aca274d013a27d6ab0104b5d52643af5682e4e
Benjamin Black <benblack86@gmail.com>
2012-03-04 10:09:42 -0500

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bodytics::Application.config.secret_token = '9e2df535dca41c9195c56a5beb5432c50729956ad2d952022d9e1334286ea5fc918fc94d3ce188157f59015cc2788609de821d7af34184d3b8296170cbc7c16c'
